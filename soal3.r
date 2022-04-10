4883bed8cd2e07a178cdbfce41abd9fe01db6e73
afiqakraam <81454701+afiqakraam@users.noreply.github.com>
2022-04-10 19:15:15 +0700

    Add files via upload

---
+++ soal3.r
@@ -0,0 +1,13 @@
+#A
+dpois(6, 4.5)
+
+#B
+hist(rpois(365, 4.5), main = "Histogram Poisson")
+
+#C
+#Berdasarkan output yang diberikan, nilai simulasi perkiraaan mendekati nilai exact.
+
+#D
+mean = var = lambda
+mean
+var
